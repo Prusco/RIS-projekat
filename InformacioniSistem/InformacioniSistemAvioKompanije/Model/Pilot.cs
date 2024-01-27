49b487848c0e3627c8a2ed6999f49856bc8e6b53
Imad <135968575+Prusco@users.noreply.github.com>
2024-01-27 17:27:16 +0100

    Add files via upload

---
+++ InformacioniSistem/InformacioniSistemAvioKompanije/Model/Pilot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformacioniSistemAvioKompanije.Model
+{
+    public class Pilot
+    {
+        public int ID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public int godineIskustva {  get; set; }
+
+        public Pilot()
+        {
+
+        }
+    }
+}
